ed59c62d3bdcabce9d448217a8b532f88d715674
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2016-11-01 12:43:22 +0000

    Add sass test

--- test/test.spec.js
+++ test/test.spec.js
@@ -33,7 +33,49 @@ it('should chunk up CSS into multiple files', done => {
       module: {
         loaders: [{
           test: /\.css$/,
-          loader: extractCSS.extract(['css'])
+          loader: extractCSS.extract(['css-loader'])
+        }]
+      },
+      plugins: [
+        new BlessCSSWebpackPlugin(),
+        extractCSS
+      ]
+    };
+
+    webpack(webpackConfig, (err, stats) => {
+
+      if (err) {
+        done(err);
+      } else {
+        expect(stats.toJson().assets[1].name).to.equal('main.css');
+        expect(stats.toJson().assets[2].name).to.equal('main-blessed1.css');
+        expect(stats.compilation.assets['main.css'].source()).to.equal(main1);
+        expect(stats.compilation.assets['main-blessed1.css'].source()).to.equal(main2);
+        done();
+      }
+
+    });
+  }).catch(done);
+
+});
+
+it('should compile SASS, then chunk it into multiple files', done => {
+
+  Promise.all([
+    readFile(__dirname + '/fixtures/expected/main.css'),
+    readFile(__dirname + '/fixtures/expected/main-blessed1.css')
+  ]).then(([main1, main2]) => {
+    const extractCSS = new ExtractTextPlugin('[name].css');
+
+    const webpackConfig = {
+      entry: __dirname + '/fixtures/scss/entry.js',
+      output: {
+        path: os.tmpdir() + '/output',
+      },
+      module: {
+        loaders: [{
+          test: /\.scss$/,
+          loader: extractCSS.extract(['css-loader', 'sass-loader'])
         }]
       },
       plugins: [
